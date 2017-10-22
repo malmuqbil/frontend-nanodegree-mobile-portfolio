8b26cc558a59270267c41c79f65def41ab54d64f
malmuqbil <il-1@hotmail.com>
2017-10-22 21:03:15 +0300

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -507,7 +507,9 @@ function updatePositions() {
 
   var items = document.querySelectorAll('.mover');
   
+    // I had created this varible out the scoop 
    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var phase;
   for (var i = 0, phase, len = items.length; i < len; i++) {
     phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
